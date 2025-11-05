72bdb107f3afa167c6446ed05c94afbe641a5389
Hoag11 <nghuyhoang1101@gmail.com>
2025-11-05 13:34:47 +0700

    update confluent

--- connect.sh
+++ connect.sh
@@ -1,2 +1,3 @@
-curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" \
-  localhost:8083/connectors/ -d @connector/connector.json
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d @connector/app-connector.json
+
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d @connector/bi-sink.json
